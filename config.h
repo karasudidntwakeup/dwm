791bc930d93479b2f58ac8dc3307f449b091d6f6
karasudidntwakeup <hemdanoovic@gmail.com>
2023-12-12 05:01:39 +0200

    bar

--- config.h
+++ config.h
@@ -11,7 +11,7 @@ static const unsigned int gappiv    = 15;       /* vert inner gap between window
 static const unsigned int gappoh    = 15;       /* horiz outer gap between windows and screen edge */
 static const unsigned int gappov    = 15;       /* vert outer gap between windows and screen edge */
 static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
-static const int showbar            = 1;        /* 0 means no bar */
+static const int showbar            = 0;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[] = { "Cascadia Mono:size=13", "SF Arabic:size=13","JetBrains Mono:size=11"};
 static const char dmenufont[]       = "Cascadia Code:size=13";
