920d38d555376697de831c4356e9328029d11485
karasudidntwakeup <hemdanoovic@gmail.com>
2023-12-22 05:00:29 +0200

    floorp is my best friend now ?

--- config.h
+++ config.h
@@ -138,7 +138,8 @@ static const char *filebrowsercmd[]  = { "pcmanfm", NULL};
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", norm_bg, "-nf", norm_fg, "-sb", sel_bg, "-sf", sel_fg, NULL };
 static const char *termcmd[]  = { "st", NULL };
-static const char *browser[]       = { "librewolf", NULL, "librewolf" };
+static const char *browser[]       = { "floorp", NULL, "floorp" };
+
 
 
 static const Key keys[] = {
