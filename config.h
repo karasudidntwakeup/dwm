8821e1d66f8d290e3aa465004dab2f19e96d83a8
karasudidntwakeup <hemdanoovic@gmail.com>
2024-01-26 04:34:08 +0200

    ;-;

--- config.h
+++ config.h
@@ -1,8 +1,5 @@
 /* See LICENSE file for copyright and license details. */
 #include <X11/XF86keysym.h>
-
-
-
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
@@ -14,8 +11,7 @@ static       int smartgaps          = 0;        /* 1 means no outer gap when the
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[] = { "Cascadia Mono:size=12", "SF Arabic:size=11","JetBrains Mono NerdFont:size=10"};
-static const char dmenufont[]       = "Cascadia Code:size=11";
-
+static const char dmenufont[]  = "Cascadia Code:size=11";
 static const char norm_fg[] = "#f1f1f1";
 static const char norm_bg[] = "#191919";
 static const char norm_border[] = "#448cf6";
@@ -27,51 +23,22 @@ static const char urg_bg[] = "#707070";
 static const char urg_border[] = "#707070";
 static const char *colors[][3]      = {
     /*               fg           bg         border                         */
-    [SchemeNorm] = { norm_fg,     norm_bg,   norm_border }, // unfocused wins
-    [SchemeSel]  = { sel_fg,      sel_bg,    sel_border },  // the focused win
-    [SchemeUrg] =  { urg_fg,      urg_bg,    urg_border },
-};
-
-
-
-//
-
-
+  [SchemeNorm] = { norm_fg,     norm_bg,   norm_border }, // unfocused wins
+  [SchemeSel]  = { sel_fg,      sel_bg,    sel_border },  // the focused win
+  [SchemeUrg] =  { urg_fg,      urg_bg,    urg_border },
+ };
 /*sound*/
-static const char *upvol[]   = { "/usr/bin/pactl", "set-sink-volume", "0", "+5%",     NULL };
-static const char *downvol[] = { "/usr/bin/pactl", "set-sink-volume", "0", "-5%",     NULL };
-static const char *mutevol[] = { "/usr/bin/pactl", "set-sink-mute",   "0", "toggle",  NULL };
-
-
-//
-
-
-//lock
-
-
-
-static const char *slockcmd[] = { "slock", NULL };
-
-
-//
-
-
+ static const char *upvol[]   = { "/usr/bin/pactl", "set-sink-volume", "0", "+5%",     NULL };
+ static const char *downvol[] = { "/usr/bin/pactl", "set-sink-volume", "0", "-5%",     NULL };
+ static const char *mutevol[] = { "/usr/bin/pactl", "set-sink-mute",   "0", "toggle",  NULL };
+//slock
+ static const char *slockcmd[] = { "slock", NULL };
 //brightness
-
-
-static const char *brightnessup[] = { "xbacklight", "-inc","5", NULL };
-static const char *brightnessdown[] = { "xbacklight", "-dec", "5", NULL };
-
-
-
-
-
+ static const char *brightnessup[] = { "xbacklight", "-inc","5", NULL };
+ static const char *brightnessdown[] = { "xbacklight", "-dec", "5", NULL };
 ///tagging */
-
-
-static const char *tags[] = { "  ", "  表 ", " 地 ", " 新 ", " 女 " };
-
-static const char *tagsel[][2] = {
+ static const char *tags[] = { "  ", "  表 ", " 地 ", " 新 ", " 女 " };
+ static const char *tagsel[][2] = {
 	{ "#be95ff", "#191919" },
 	{ "#f5ce6b", "#191919" },
 	{ "#ee5396", "#191919" },
@@ -81,27 +48,20 @@ static const char *tagsel[][2] = {
 	{ "#ffffff", "#191919" },
 	{ "#b4aefe", "#191919" },
 	{ "#ee5396", "#191919" },
-};
-
+ };
 static const Rule rules[] = {
 	/* xprop(1):
 	 *	WM_CLASS(STRING) = instance, class
-	 *	WM_NAME(STRING) = title
-	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "qutebrowser",  NULL,       NULL,       1 << 8,       0,           -1 },
-};
-
+	 *	WM_NAME(STRING) = title */
+{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
+ };
 /* layout(s) */
 static const float mfact     = 0.5; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
-
 #define FORCE_VSPLIT 1  /* nrowgrid layout: force two clients to always split vertically */
 #include "vanitygaps.c"
-
 static const Layout layouts[] = {
 	/* symbol     arrange function */
 	{ "[]",      spiral },
@@ -120,7 +80,6 @@ static const Layout layouts[] = {
 	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ NULL,       NULL },
 };
-
 /* key definitions */
 #define MODKEY Mod4Mask
 #define TAGKEYS(KEY,TAG) \
@@ -128,31 +87,21 @@ static const Layout layouts[] = {
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
 	{ MODKEY|ShiftMask,             KEY,      tag,            {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
-
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
-
 /* commands */
-
 static const char *filebrowsercmd[]  = { "pcmanfm", NULL};
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", norm_bg, "-nf", norm_fg, "-sb", sel_bg, "-sf", sel_fg, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *browser[]       = { "librewolf", NULL, "librewolf" };
-
-
-
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
-
-
-
   { MODKEY,             XK_Return, spawn,          {.v = termcmd } },
         { MODKEY,                       XK_g,      setlayout,      {.v = &layouts[0] } },
         { MODKEY,                       XK_f,      spawn,       {.v = filebrowsercmd } },
         { MODKEY,                       XK_space,  spawn,          {.v = dmenucmd} },
         { MODKEY,                       XK_c,      spawn,       {.v = browser } },
-
         { MODKEY,                       XK_b,      togglebar,      {0} },
         { MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
         { MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
@@ -186,12 +135,11 @@ static const Key keys[] = {
         { MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
         { MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
         { MODKEY|ShiftMask,             XK_w,  togglefloating, {0} },
-        { 0,                            XK_Print,                   spawn,
-SHCMD("maim -s -d 1 | xclip -selection clipboard -t image/png -i") },
-
+        { 0,                            XK_Print,                   spawn,SHCMD("maim -s -d 1 | xclip -selection clipboard -t image/png -i") },
+        { MODKEY,                       XK_s,                       spawn,SHCMD("telegram-desktop") },
+        { MODKEY,                       XK_z,                       spawn,SHCMD("ungoogled-chromium") },
         { MODKEY,                       XK_0,      view,           {.ui = ~0 } },
-                                                                  
-  { MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
+        { MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
         { MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
         { MODKEY,                       XK_period, focusmon,       {.i = +1 } },
         { MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
@@ -202,36 +150,18 @@ SHCMD("maim -s -d 1 | xclip -selection clipboard -t image/png -i") },
         TAGKEYS(                        XK_4,                      3)
         TAGKEYS(                        XK_5,                      4)
         TAGKEYS(                        XK_6,                      5)
-        TAGKEYS(                        XK_7,                      6)
-        TAGKEYS(                        XK_8,                      7)
-        TAGKEYS(                        XK_9,                      8)
         { MODKEY|ShiftMask,             XK_q,      quit,           {0} },
-
-
-
+        { MODKEY,                       XK_n,      spawn,               {.v = slockcmd } },
         { 0,                       XF86XK_AudioLowerVolume, spawn, {.v = downvol } },
         { 0,                       XF86XK_AudioMute, spawn, {.v = mutevol } },
         { 0,                       XF86XK_AudioRaiseVolume, spawn, {.v = upvol   } },
-
-
-
-
-         { 0, XF86XK_MonBrightnessUp,   spawn,                           {.v = brightnessup} },
-         { 0, XF86XK_MonBrightnessDown, spawn,                           {.v = brightnessdown} },
-
-
-         { 0,                            XK_F5,     spawn,               SHCMD("setbg") },
-
-         { MODKEY,                       XK_n,      spawn,               {.v = slockcmd } },
-
-
-	{ MODKEY|ShiftMask,             XK_h,      setcfact,       {.f = +0.25} },
-	{ MODKEY|ShiftMask,             XK_l,      setcfact,       {.f = -0.25} },
-	{ MODKEY|ShiftMask,             XK_o,      setcfact,       {.f =  0.00} },
- 
-
+        { 0, XF86XK_MonBrightnessUp,   spawn,                           {.v = brightnessup} },
+        { 0, XF86XK_MonBrightnessDown, spawn,                           {.v = brightnessdown} },
+        { MODKEY,                       XK_n,      spawn,               {.v = slockcmd } },
+	      { MODKEY|ShiftMask,             XK_h,      setcfact,       {.f = +0.25} },
+      	{ MODKEY|ShiftMask,             XK_l,      setcfact,       {.f = -0.25} },
+	      { MODKEY|ShiftMask,             XK_o,      setcfact,       {.f =  0.00} },
 };
-
 /* button definitions */
 /* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
 static const Button buttons[] = {
