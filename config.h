f23e0eabf75e126d5af267f1f62e02ce7f29efab
karasudidntwakeup <karasu@steering.nextdns.io>
2024-07-19 22:59:47 +0300

    Artix

--- config.h
+++ config.h
@@ -93,11 +93,11 @@ static const Layout layouts[] = {
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 /* commands */
-static const char *filebrowsercmd[]  = { "pcmanfm", NULL};
+static const char *filebrowsercmd[]  = { "yazi", NULL};
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run" };
 static const char *termcmd[]  = { "st", NULL };
-static const char *browser[]       = { "chromium", NULL, "chromium" };
+static const char *browser[]       = { "librewolf", NULL, "librewolf" };
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
   { MODKEY,             XK_Return, spawn,          {.v = termcmd } },
@@ -105,7 +105,7 @@ static const Key keys[] = {
         { MODKEY,                       XK_g,      setlayout,      {.v = &layouts[0] } },
         { MODKEY,                       XK_f,      spawn,       {.v = filebrowsercmd } },
         { MODKEY,                       XK_space,  spawn,          {.v = dmenucmd} },
-        { MODKEY,                       XK_z,      spawn,       {.v = browser } },
+        { MODKEY,                       XK_c,      spawn,       {.v = browser } },
         { MODKEY,                       XK_a,      togglebar,      {0} },
         { MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
         { MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
@@ -141,7 +141,6 @@ static const Key keys[] = {
         { MODKEY|ShiftMask,             XK_w,      togglefloating, {0} },
         { 0,                            XK_Print,                   spawn,SHCMD("maim -o -s -d 1 | xclip -selection clipboard -t image/png -i") },
         { MODKEY,                       XK_s,                       spawn,SHCMD("telegram-desktop") },
-        { MODKEY,                       XK_c,                       spawn,SHCMD("code-oss") },
         { MODKEY,                       XK_0,      view,           {.ui = ~0 } },
         { MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
         { MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
