a6c68cd59525dfc12f4b94aee0bf6193dd5db080
karasudidntwakeup <hemdanoovic@gmail.com>
2024-12-29 21:44:22 +0200

    libre

--- config.h
+++ config.h
@@ -140,7 +140,7 @@ static const Key keys[] = {
         { MODKEY|ShiftMask,             XK_w,      togglefloating, {0} },
         { 0,                            XK_Print,                   spawn,SHCMD("maim -o -s -d 1 | xclip -selection clipboard -t image/png -i") },
         { MODKEY,                       XK_s,                       spawn,SHCMD("telegram-desktop") },
-        { MODKEY,                       XK_z,                       spawn,SHCMD("firefox") },
+        { MODKEY,                       XK_z,                       spawn,SHCMD("librewolf") },
         { MODKEY,                       XK_0,      view,           {.ui = ~0 } },
         { MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
         { MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
